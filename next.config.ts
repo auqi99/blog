1909ad954d2cfdc2550fac823b822ba5adc87c62
auqi99 <aulbg99@gmail.com>
2025-03-11 15:02:27 +0700

    additional

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_CONTENTFUL_SPACE_ID:
+      process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN:
+      process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  },
 };
 
 export default nextConfig;
